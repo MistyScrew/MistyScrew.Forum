959998db81edfcdb1d61f20008975c0d15ea4c02
Anna <annacrab7@gmail.com>
2019-12-07 11:40:19 +0300

    Counts and date aligned

--- MistyScrew.Forum.Wui/ClientApp/src/controls/BoardsView.tsx
+++ MistyScrew.Forum.Wui/ClientApp/src/controls/BoardsView.tsx
@@ -21,13 +21,13 @@ export function BoardsView(props: { areas: Area[] }) {
                   <Link to={`board/${oc(board).name('')}`}>{oc(board).title('')}</Link><br />
                   <span className='description'>{oc(board).description('')}</span>
                 </td>
-                <td>
+                <td className='td-right'>
                   {oc(board).state.threadCount()}
                 </td>
-                <td>
+                <td className='td-right'>
                   {oc(board).state.postCount()}
                 </td>
-                <td>
+                <td className='td-center'>
                   {moment(oc(board).state.lastPost.time('')).format("DD.MM.YYYY HH:mm:ss")}
                 </td>
                 <td className='td-moderators'>
